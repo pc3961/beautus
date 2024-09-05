fab9a939b247e490496c99d214628dc0e3bf4dd1
PC3961 <cpranavss65@gmail.com>
2024-09-05 14:41:02 +0530

    Updated Forms and Added Email Otp Authentication

--- inc/mailer.php
+++ inc/mailer.php
@@ -6,6 +6,8 @@ require './mail/Exception.php';
 require './mail/PHPMailer.php';
 require './mail/SMTP.php';
 
+session_start();  // Start session to store OTP
+
 header('Content-Type: application/json');  // Set response to JSON format
 $response = ['status' => 'error', 'message' => 'An error occurred.'];  // Default response
 
@@ -26,38 +28,86 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
 
-    // reCAPTCHA successful, proceed with mailer
-    $mail = new PHPMailer(true);
-    try {
-        // Server settings
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';  // Set your SMTP server
-        $mail->SMTPAuth = true;
-        $mail->Username = 'test@omesacreative.ca';  // SMTP username
-        $mail->Password = 'ojgavpiqkxixbqpz';  // SMTP password
-        $mail->SMTPSecure = 'ssl';
-        $mail->Port = 465;
+    // Check if OTP is being submitted
+    if (isset($_POST['otp'])) {
+        // Verify OTP
+        if ($_POST['otp'] == $_SESSION['otp']) {
+            // OTP is valid, proceed to send the final email
+            $mail = new PHPMailer(true);
+            try {
+                // Server settings
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com';  // Set your SMTP server
+                $mail->SMTPAuth = true;
+                $mail->Username = 'test@omesacreative.ca';  // SMTP username
+                $mail->Password = 'ojgavpiqkxixbqpz';  // SMTP password
+                $mail->SMTPSecure = 'ssl';
+                $mail->Port = 465;
 
-        // Recipients
-        $mail->setFrom('test@omesacreative.ca', 'Test Omesacreative');
-        $mail->addAddress('pranav@cgstechlab.com', 'Pranav Chavan');  // Add recipient
+                // Recipients
+                $mail->setFrom('test@omesacreative.ca', 'Test Omesacreative');
+                $mail->addAddress('pranav@cgstechlab.com', 'Pranav Chavan');  // Add recipient
 
-        // Content
-        $mail->isHTML(true);
-        $mail->Subject = 'New Contact Form Submission';
-        $mail->Body = '<b>First Name:</b> ' . $_POST['fname'] . '<br><b>Last Name:</b> ' . $_POST['lname'] . '<br><b>Email:</b> ' . $_POST['bemail'] . '<br><b>Phone Number:</b> ' . $_POST['pnum'] . '<br><b>Company Name:</b> ' . $_POST['cname'] . '<br><b>Message:</b><br>' . nl2br($_POST['msg']);
-        $mail->AltBody = 'First Name: ' . $_POST['fname'] . "\nLast Name: " . $_POST['lname'] . "\nEmail: " . $_POST['bemail'] . "\nPhone Number: " . $_POST['pnum'] . "\nCompany Name: " . $_POST['cname'] . "\nMessage:\n" . $_POST['msg'];
+                // Content
+                $mail->isHTML(true);
+                $mail->Subject = 'New Contact Form Submission';
+                $mail->Body = '<b>First Name:</b> ' . $_POST['fname'] . '<br><b>Last Name:</b> ' . $_POST['lname'] . '<br><b>Email:</b> ' . $_POST['bemail'] . '<br><b>Phone Number:</b> ' . $_POST['pnum'] . '<br><b>Company Name:</b> ' . $_POST['cname'] . '<br><b>Message:</b><br>' . nl2br($_POST['msg']);
+                $mail->AltBody = 'First Name: ' . $_POST['fname'] . "\nLast Name: " . $_POST['lname'] . "\nEmail: " . $_POST['bemail'] . "\nPhone Number: " . $_POST['pnum'] . "\nCompany Name: " . $_POST['cname'] . "\nMessage:\n" . $_POST['msg'];
 
-        // Send the email
-        $mail->send();
+                // Send the email
+                $mail->send();
 
-        // If mail is sent successfully, return success response
-        $response['status'] = 'success';
-        $response['message'] = 'We are excited to hear from you! We will connect to discuss more about your interest.';
+                // If mail is sent successfully, return success response
+                $response['status'] = 'success';
+                $response['message'] = 'We are excited to hear from you! We will connect to discuss more about your interest.';
 
-    } catch (Exception $e) {
-        // If there was an error sending the email
-        $response['message'] = 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+            } catch (Exception $e) {
+                // If there was an error sending the email
+                $response['message'] = 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+            }
+        } else {
+            // Invalid OTP
+            $response['message'] = 'Invalid OTP. Please try again.';
+        }
+
+    } else {
+        // Generate OTP and send it to the user's email
+        $otp = rand(100000, 999999);  // Generate a random 6-digit OTP
+        $_SESSION['otp'] = $otp;  // Store OTP in the session
+
+        // Send OTP to the email
+        $mail = new PHPMailer(true);
+        try {
+            // Server settings
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';  // Set your SMTP server
+            $mail->SMTPAuth = true;
+            $mail->Username = 'test@omesacreative.ca';  // SMTP username
+            $mail->Password = 'ojgavpiqkxixbqpz';  // SMTP password
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+
+            // Recipients
+            $mail->setFrom('test@omesacreative.ca', 'Test Omesacreative');
+            $mail->addAddress($_POST['bemail'], $_POST['fname'] . ' ' . $_POST['lname']);  // Send OTP to user's email
+
+            // Content
+            $mail->isHTML(true);
+            $mail->Subject = 'Your OTP for form submission';
+            $mail->Body = '<p>Your OTP is <b>' . $otp . '</b>. Please enter this OTP to verify your email address.</p>';
+            $mail->AltBody = 'Your OTP is ' . $otp . '. Please enter this OTP to verify your email address.';
+
+            // Send the email
+            $mail->send();
+
+            // If OTP is sent successfully, return success response
+            $response['status'] = 'otp_sent';
+            $response['message'] = 'An OTP has been sent to your email. Please enter it to proceed.';
+
+        } catch (Exception $e) {
+            // If there was an error sending the OTP
+            $response['message'] = 'OTP could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+        }
     }
 
     // Return the JSON response
