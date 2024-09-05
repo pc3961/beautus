774832399fc3b1155c3a7317f9a69f631d7fce5c
PC3961 <cpranavss65@gmail.com>
2024-09-05 14:07:29 +0530

    Updated Font to Urbanist Font Theme

--- inc/mailer.php
+++ inc/mailer.php
@@ -5,64 +5,62 @@ use PHPMailer\PHPMailer\Exception;
 require './mail/Exception.php';
 require './mail/PHPMailer.php';
 require './mail/SMTP.php';
-$mail = new PHPMailer(true);
 
-try {
-    // Server settings
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com'; // Set your SMTP server
-    $mail->SMTPAuth = true;
-    $mail->Username = 'test@omesacreative.ca'; // SMTP username
-    $mail->Password = 'ojgavpiqkxixbqpz'; // SMTP password
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port = 465;
-
-    // Recipients
-    $mail->setFrom('test@omesacreative.ca', 'Test Omesacreative');
-    $mail->addAddress('shailesh@wickedwipes.ca', 'Shailesh Merai');
-
-    // Content
-    $mail->isHTML(true);
-    $mail->Subject = 'New Contact Form Submission';
-    $mail->Body = '<b>First Name:</b> ' . $_POST['fname'] . '<br><b>Last Name:</b> ' . $_POST['lname'] . '<br><b>Email:</b> ' . $_POST['bemail'] . '<br><b>Phone Number:</b> ' . $_POST['pnum'] . '<br><b>Company Name:</b> ' . $_POST['cname'] . '<br><b>Message:</b><br>' . nl2br($_POST['msg']);
-    $mail->AltBody = 'First Name: ' . $_POST['fname'] . "\nLast Name: " . $_POST['lname'] . "\nEmail: " . $_POST['bemail'] . "\nPhone Number: " . $_POST['pnum'] . "\nCompany Name: " . $_POST['cname'] . "\nMessage:\n" . $_POST['msg'];
-
-    $mail->send();
-    echo 'Message has been sent';
-
-    // //Thank you Email
-    // $mail->clearAddresses(); // Clear all addresses for the next email
-    // $mail->addAddress($_POST['bemail']); // Send thank you email to the user's email
-    // $mail->Subject = 'Thank you for showing interest!';
-    // $mail->Body = 'Dear ' . $_POST['fname'] . ' ' . $_POST['lname'] . ',<br><br>' .
-    //     'Thank you for showing interest in our services. We have received your message and will get back to you shortly.<br><br>' .
-    //     'Best regards,<br>' .
-    //     'Wick Wipes'; // Customize with your company name
-    // $mail->AltBody = 'Dear ' . $_POST['fname'] . ' ' . $_POST['lname'] . ",\n\n" .
-    //     "Thank you for showing interest in our services. We have received your message and will get back to you shortly.\n\n" .
-    //     "Best regards,\n" .
-    //     "Wicked Wipes"; // Customize with your company name
-
-    // $mail->send();
-    // echo 'Thank you message has been sent';
-
-} catch (Exception $e) {
-    echo 0;
-}
+header('Content-Type: application/json');  // Set response to JSON format
+$response = ['status' => 'error', 'message' => 'An error occurred.'];  // Default response
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $recaptcha_secret = '6LeK3DAqAAAAAG8yuTlWzVS_g-P1QFy1w4PC-pHd'; // Replace with your actual secret key
+    // reCAPTCHA secret key
+    $recaptcha_secret = '6LeK3DAqAAAAAG8yuTlWzVS_g-P1QFy1w4PC-pHd';  // Replace with your actual secret key
     $recaptcha_response = $_POST['g-recaptcha-response'];
 
-    // Make a POST request to the reCAPTCHA API
-    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$recaptcha_secret&response=$recaptcha_response");
-    $response_keys = json_decode($response, true);
+    // Verify the reCAPTCHA response with Google
+    $recaptcha_verify_url = "https://www.google.com/recaptcha/api/siteverify?secret=$recaptcha_secret&response=$recaptcha_response";
+    $recaptcha_verify_response = file_get_contents($recaptcha_verify_url);
+    $response_keys = json_decode($recaptcha_verify_response, true);
 
     if (intval($response_keys["success"]) !== 1) {
-        echo 'Please complete the CAPTCHA.';
-    } else {
-        echo 1;
-        // Continue with form processing
+        // reCAPTCHA failed
+        $response['message'] = 'Please complete the CAPTCHA.';
+        echo json_encode($response);
+        exit;
     }
+
+    // reCAPTCHA successful, proceed with mailer
+    $mail = new PHPMailer(true);
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';  // Set your SMTP server
+        $mail->SMTPAuth = true;
+        $mail->Username = 'test@omesacreative.ca';  // SMTP username
+        $mail->Password = 'ojgavpiqkxixbqpz';  // SMTP password
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+
+        // Recipients
+        $mail->setFrom('test@omesacreative.ca', 'Test Omesacreative');
+        $mail->addAddress('pranav@cgstechlab.com', 'Pranav Chavan');  // Add recipient
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'New Contact Form Submission';
+        $mail->Body = '<b>First Name:</b> ' . $_POST['fname'] . '<br><b>Last Name:</b> ' . $_POST['lname'] . '<br><b>Email:</b> ' . $_POST['bemail'] . '<br><b>Phone Number:</b> ' . $_POST['pnum'] . '<br><b>Company Name:</b> ' . $_POST['cname'] . '<br><b>Message:</b><br>' . nl2br($_POST['msg']);
+        $mail->AltBody = 'First Name: ' . $_POST['fname'] . "\nLast Name: " . $_POST['lname'] . "\nEmail: " . $_POST['bemail'] . "\nPhone Number: " . $_POST['pnum'] . "\nCompany Name: " . $_POST['cname'] . "\nMessage:\n" . $_POST['msg'];
+
+        // Send the email
+        $mail->send();
+
+        // If mail is sent successfully, return success response
+        $response['status'] = 'success';
+        $response['message'] = 'We are excited to hear from you! We will connect to discuss more about your interest.';
+
+    } catch (Exception $e) {
+        // If there was an error sending the email
+        $response['message'] = 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+    }
+
+    // Return the JSON response
+    echo json_encode($response);
 }
 ?>
